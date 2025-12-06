ba3d21b9f481a05987bf6ccb583992986ce981da
Etich <avfetich@gmail.com>
2025-12-06 15:49:39 +0300

    remove test stuff

--- RealmServer/TestPacketGen/Text.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace GameServer.Game.Network.Messaging.Outgoing;
-
-public readonly partial record struct Text(string Name, int ObjId, int NumStars, byte BubbleTime, string Recipent, string Txt) : IOutgoingPacket
-{
-    static PacketId IOutgoingPacket.PacketId => PacketId.TEXT;
-}
