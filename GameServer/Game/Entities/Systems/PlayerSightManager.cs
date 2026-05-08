c8c9c8ca3a09cfc9b5bc2132c4c099ba1e89d145
Zolmex <zolmexyt@gmail.com>
2026-05-08 00:06:06 -0300

    Reduce player sight allocations

--- GameServer/Game/Entities/Systems/PlayerSightManager.cs
+++ GameServer/Game/Entities/Systems/PlayerSightManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Common;
@@ -19,35 +20,34 @@ public class PlayerSightManager(World world, int capacity) : ManagerBase<PlayerS
     public const int SIGHT_RADIUS = 20;
     public const int SIGHT_RADIUS_SQR = SIGHT_RADIUS * SIGHT_RADIUS;
     
+    private MapTileData[] _newTiles = ArrayPool<MapTileData>.Shared.Rent(50);
+    private ObjectData[] _newEntities = ArrayPool<ObjectData>.Shared.Rent(50);
+    
     public override void Tick(ref RealmTime time) {
-        var newTiles = new List<MapTileData>(50);
-        var newEntities = new List<ObjectData>(50);
         foreach (ref var sight in this) {
             ref var playerStats = ref _world.EntityStats.Get(sight.Id);
             if (playerStats.Id == 0)
                 continue;
             
             var user = _world.PlayerToUser[playerStats.Id];
-            ProcessUpdate(user, ref playerStats, ref sight, newTiles, newEntities);
+            ProcessUpdate(user, ref playerStats, ref sight);
             ProcessNewtick(user, playerStats.Id, ref sight);
         }
     }
 
-    private void ProcessUpdate(User user, ref EntityStats playerEntityStats, ref PlayerSight sight, List<MapTileData> newTiles, List<ObjectData> newEntities) {
-        GetNewTiles(ref playerEntityStats, ref sight, newTiles);
-        GetNewEntities(ref playerEntityStats, ref sight, newEntities);
+    private void ProcessUpdate(User user, ref EntityStats playerEntityStats, ref PlayerSight sight) {
+        var newTileCount = GetNewTiles(ref playerEntityStats, ref sight);
+        var newEntityCount = GetNewEntities(ref playerEntityStats, ref sight);
 
-        if (newTiles.Count == 0 && newEntities.Count == 0)
+        if (newTileCount == 0 && newEntityCount == 0)
             return;
         
-        user.SendPacket(new Update(CollectionsMarshal.AsSpan(newTiles), CollectionsMarshal.AsSpan(newEntities),
-            Span<ObjectDropData>.Empty));
-        
-        newTiles.Clear();
-        newEntities.Clear();
+        user.SendPacket(new Update(_newTiles.AsSpan(), newTileCount, _newEntities.AsSpan(), newEntityCount,
+            Span<ObjectDropData>.Empty, 0));
     }
     
-    private void GetNewTiles(ref EntityStats playerEntityStats, ref PlayerSight sight, List<MapTileData> newTiles) {
+    private int GetNewTiles(ref EntityStats playerEntityStats, ref PlayerSight sight) {
+        var newTileCount = 0;
         sight.VisibleTiles.Clear();
         switch (_world.Config.Blocksight) {
             case World.UNBLOCKED_SIGHT:
@@ -62,19 +62,29 @@ public class PlayerSightManager(World world, int capacity) : ManagerBase<PlayerS
                             var tile = _world.Map[x, y];
 
                             sight.VisibleTiles.Add(tile.Pos);
-                            if (sight.DiscoveredTiles.Add(tile.Pos)) // This is a newly discovered tile
-                                newTiles.Add(tile);
+                            if (sight.DiscoveredTiles.Add(tile.Pos)) { // This is a newly discovered tile
+                                _newTiles[newTileCount++] = tile;
+                                if (newTileCount >= _newTiles.Length) {
+                                    var newArr = ArrayPool<MapTileData>.Shared.Rent(newTileCount * 2);
+                                    _newTiles.AsSpan().CopyTo(newArr);
+                                    ArrayPool<MapTileData>.Shared.Return(_newTiles);
+                                    _newTiles = newArr;
+                                }
+                            }
                         }
 
                 break;
         }
+
+        return newTileCount;
     }
 
-    private void GetNewEntities(ref EntityStats playerEntityStats, ref PlayerSight sight, List<ObjectData> newEntities) {
+    private int GetNewEntities(ref EntityStats playerEntityStats, ref PlayerSight sight) {
+        var newEntityCount = 0;
         foreach (ref var en in _world.Map.GetEntitiesWithin(playerEntityStats.Pos, SIGHT_RADIUS_SQR)) {
             ref var stats = ref _world.EntityStats.Get(en.Id);
             if (sight.VisibleEntities.Add(en.Id)) {
-                newEntities.Add(new ObjectData() {
+                _newEntities[newEntityCount++] = new ObjectData() {
                     ObjectType = en.ObjectType,
                     PrivateMask = en.Id == playerEntityStats.Id ? stats.PrivateMask : stats.PublicMask,
                     Status = new ObjectStatusData() {
@@ -82,7 +92,14 @@ public class PlayerSightManager(World world, int capacity) : ManagerBase<PlayerS
                         Pos = stats.Pos,
                         Stats = stats.Stats
                     }
-                });
+                };
+                if (newEntityCount >= _newEntities.Length) {
+                    var newArr = ArrayPool<ObjectData>.Shared.Rent(newEntityCount * 2);
+                    _newEntities.AsSpan().CopyTo(newArr);
+                    ArrayPool<ObjectData>.Shared.Return(_newEntities);
+                    _newEntities = newArr;
+                }
+                
                 sight.Statuses.Add(new ObjectStatusData() {
                     ObjectId = en.Id,
                     Pos = stats.Pos,
@@ -90,6 +107,8 @@ public class PlayerSightManager(World world, int capacity) : ManagerBase<PlayerS
                 });
             }
         }
+
+        return newEntityCount;
     }
 
     private void ProcessNewtick(User user, int playerId, ref PlayerSight sight) {
--- GameServer/Game/Network/Messaging/Outgoing/Update.cs
+++ GameServer/Game/Network/Messaging/Outgoing/Update.cs
@@ -10,27 +10,33 @@ public readonly ref struct Update : IOutgoingPacket {
     public PacketId ID => PacketId.UPDATE;
 
     private readonly Span<MapTileData> _tiles;
+    private readonly int _tileCount;
     private readonly Span<ObjectData> _newEntities;
+    private readonly int _newEntityCount;
     private readonly Span<ObjectDropData> _oldEntities;
+    private readonly int _dropCount;
     
     public Update(
-        Span<MapTileData> tiles,
-        Span<ObjectData> newEntities,
-        Span<ObjectDropData> oldEntities) {
+        Span<MapTileData> tiles, int newTileCount,
+        Span<ObjectData> newEntities, int newEntityCount,
+        Span<ObjectDropData> oldEntities, int dropCount) {
         _tiles = tiles;
+        _tileCount = newTileCount;
         _newEntities = newEntities;
+        _newEntityCount = newEntityCount;
         _oldEntities = oldEntities;
+        _dropCount = dropCount;
     }
 
     public void Write(ref SpanWriter wtr) {
-        wtr.Write((short)_tiles.Length);
-        for (var i = 0; i < _tiles.Length; i++)
+        wtr.Write((short)_tileCount);
+        for (var i = 0; i < _tileCount; i++)
             _tiles[i].Write(ref wtr);
-        wtr.Write((short)_newEntities.Length);
-        for (var i = 0; i < _newEntities.Length; i++)
+        wtr.Write((short)_newEntityCount);
+        for (var i = 0; i < _newEntityCount; i++)
             _newEntities[i].Write(ref wtr);
-        wtr.Write((short)_oldEntities.Length);
-        for (var i = 0; i < _oldEntities.Length; i++)
+        wtr.Write((short)_dropCount);
+        for (var i = 0; i < _dropCount; i++)
             _oldEntities[i].Write(ref wtr);
     }
 }
