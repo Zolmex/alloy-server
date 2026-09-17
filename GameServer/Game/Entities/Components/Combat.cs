6f6e96b4ea0eb5144fd1ffab8fd46106192f486a
Zolmex <zolmexyt@gmail.com>
2026-09-17 12:15:20 -0300

    Cleaned up Combat component

--- GameServer/Game/Entities/Components/Combat.cs
+++ GameServer/Game/Entities/Components/Combat.cs
@@ -7,31 +7,4 @@ using World = GameServer.Game.Worlds.World;
 
 namespace GameServer.Game.Entities.Components;
 
-public struct Combat {
-    
-    public int DamageReceived;
-    
-    public void Tick(World world, Entity entity, ref Stats stats) {
-        var hp = stats.GetInt(StatType.HP);
-        var newHp = hp - DamageReceived;
-        stats.Set(StatType.HP, newHp);
-
-        if (newHp <= 0)
-            Death(world, entity);
-        
-        DamageReceived = 0;
-    }
-    
-    public void ApplyDamage(int damage) {
-        DamageReceived += damage;
-    }
-    
-    private void Death(World world, Entity entity) {
-        if (world.Ecs.Has<PlayerTag>(entity)) {
-            // TODO: Spawn gravestone, announce death, register death in database
-            world.Users[entity].Disconnect(reason: DisconnectReason.Death);
-            return;
-        }
-        world.LeaveWorld(entity);
-    }
-}
+public record struct Combat(int DamageReceived) {}
--- GameServer/Game/Entities/Systems/DamageSystem.cs
+++ GameServer/Game/Entities/Systems/DamageSystem.cs
@@ -1,15 +1,22 @@
 using System.Diagnostics;
 using Arch.Core;
 using Arch.System;
+using Common;
 using Common.Game;
 using Common.Utilities.Collections;
 using GameServer.Game.Entities.Components;
+using GameServer.Game.Network;
 using GameServer.Game.Network.Messaging.Outgoing;
 using World = GameServer.Game.Worlds.World;
 
 namespace GameServer.Game.Entities.Systems;
 
-public readonly record struct DamageRecord(Entity From, Entity Target, int Damage) {
+public record struct DamageRecord(Entity From, Entity Target, int Damage) {
+
+    public void Reset() {
+        Damage = 0;
+    }
+    
     public static DamageRecord operator +(DamageRecord left, DamageRecord right)
         => left with { Damage = left.Damage + right.Damage };
 
@@ -26,11 +33,28 @@ public partial class DamageSystem(World world) : BaseSystem<World, RealmTime>(wo
         if (!_records.TryGetValue(entity, out var records))
             return;
 
-        foreach (var record in records) {
-            combat.ApplyDamage(record.Value.Damage);
+        foreach (var (en, record) in records) {
+            combat.DamageReceived += record.Damage;
+            record.Reset();
         }
         
-        combat.Tick(World, entity, ref stats);
+        var hp = stats.GetInt(StatType.HP);
+        var newHp = hp - combat.DamageReceived;
+        stats.Set(StatType.HP, newHp);
+
+        if (newHp <= 0)
+            Death(entity);
+        
+        combat.DamageReceived = 0;
+    }
+    
+    private void Death(Entity entity) {
+        if (World.Ecs.Has<PlayerTag>(entity)) {
+            // TODO: Spawn gravestone, announce death, register death in database
+            World.Users[entity].Disconnect(reason: DisconnectReason.Death);
+            return;
+        }
+        World.LeaveWorld(entity);
     }
 
     public void Damage(DamageRecord record) {
@@ -46,21 +70,8 @@ public partial class DamageSystem(World world) : BaseSystem<World, RealmTime>(wo
     }
     
     public void DamageWithText(DamageRecord record) {
-        if (!_records.TryGetValue(record.Target, out var records))
-            _records[record.Target] = records = new Dictionary<Entity, DamageRecord>();
-        
-        if (!records.TryGetValue(record.From, out var prev)) {
-            records[record.From] = record;
-            SendNotification(record.Target, record.Damage);
-            return;
-        }
-
-        records[record.From] = prev + record;
-        SendNotification(record.Target, record.Damage);
-    }
-
-    private void SendNotification(Entity entity, int damage) {
-        var user = world.Users[entity];
-        user.SendPacket(new Notification((EntityId)entity, "-" + damage, 0xFF0000, 24));
+        Damage(record);
+        var user = World.Users[record.Target];
+        user.SendPacket(new Notification((EntityId)record.Target, "-" + record.Damage, 0xFF0000, 24));
     }
 }
